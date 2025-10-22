3a0da2ab8d5e1c88b79cc0279d92c587522712cd
d-perado <sjm01065@naver.com>
2025-10-22 17:24:39 +0900

    lv3 Kiosk 생성자를 통해 리스트를 할당받도록 바꿈.

--- src/lv3/App.java
+++ src/lv3/App.java
@@ -1,10 +1,17 @@
 package lv3;
 
 
+import java.util.ArrayList;
+
 public class App {
 
     public static void main(String[] args) {
-        Kiosk kiosk = new Kiosk();
+        ArrayList<MenuItem> menuItems=new ArrayList<>();
+        menuItems.add(new MenuItem("ShackBurger", 6.9, "토마토, 양상추, 쉑소스가 토핑된 치즈버거"));
+        menuItems.add(new MenuItem("SmokeShack", 8.9, "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"));
+        menuItems.add(new MenuItem("Hamburger", 6.9, "비프패티를 기반으로 야채가 들어간 기본버거"));
+        menuItems.add(new MenuItem("Cheeseburger", 5.4, "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"));
+        Kiosk kiosk = new Kiosk(menuItems);
         kiosk.start();
     }
 }
